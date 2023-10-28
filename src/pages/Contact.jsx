65432f33f1e39534f4cec9d7f778cf3b173154aa
Benjahh <lucasbenja09@hotmail.com>
2023-10-27 22:18:25 -0300

    EmailJS and contact form implementation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { ShrinkInView } from '../utils/components/framerComponents/ShrinkInView';
 
 export const ContactPage = () => {
-  const HandleSubmit = () => {};
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_xggxubu',
+        'template_4oal3yg',
+        form.current,
+        'KB4tWR5hEnlfv4QSZ'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section className="bg-accent">
       <ShrinkInView className="page__container">
@@ -12,8 +33,20 @@ export const ContactPage = () => {
             <span className="text-accent"> contact </span> Me?
           </h1>
         </div>
-        <div className="bg-green-500">
-          <form action="Submit"></form>
+        <div className="">
+          <form
+            ref={form}
+            className="flex flex-col text-white"
+            onSubmit={sendEmail}
+          >
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+          </form>
         </div>
       </ShrinkInView>
     </section>
--- src/utils/components/shared/Footer.jsx
+++ src/utils/components/shared/Footer.jsx
@@ -4,7 +4,7 @@ import { SiGithub, SiLinkedin } from 'react-icons/si';
 
 export const Footer = () => {
   return (
-    <div className="bottom-0 h-[100px] w-full justify-between p-6 bg-purple-600 text-white flex-row flex">
+    <div className="bottom-0 h-[100px] w-full justify-between p-6 bg-black text-white flex-row flex">
       <section className={`flex-col`}>copyright things</section>
       <section className={`flex-col gap-4`}>
         links of proffesional stuff
