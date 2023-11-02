79d3402d7d93142bc982fe64480ccce057be5d5c
Benjahh <lucasbenja09@hotmail.com>
2023-11-01 23:08:08 -0300

    contact page even more styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -26,34 +26,46 @@ export const ContactPage = () => {
   };
   return (
     <ShrinkInView className="page__container">
-      <div className="page__title">
-        <h1>
-          ¿Do you want to
-          <span className="text-accent"> contact </span> Me?
-        </h1>
-      </div>
-
-      <div className="flex-between  gap-10  flex-row grow ">
-        <div className="text-white flex flex-col text-center gap-4 text-2xl">
-          <h1>Have any ideas, doubts or questions?</h1>
-          <h1>Send me an email!</h1>
+      <div className="flex-between gap-10  flex-row grow  ">
+        <div className="text-white    flex-col flex gap-10 ">
+          <h1 className="page__title top-0 ">
+            ¿Do you want to
+            <span className="text-accent"> contact </span> Me?
+          </h1>
+          <div className="text-center  font-bold gap-2 text-2xl flex flex-col">
+            <h1>¿Have any ideas, doubts or questions?</h1>
+            <h1>¡Send me an email!</h1>
+          </div>
         </div>
-        <div className="bg-primary ">
+        <div className="grow ">
           <form
             ref={form}
-            className="flex flex-col gap-2  p-28 text-white"
+            className="flex flex-col gap-8 grow text-white"
             onSubmit={sendEmail}
           >
-            <label>Name</label>
-            <input className="contact__input" type="text" name="user_name" />
-            <label>Email</label>
-            <input className="contact__input" type="email" name="user_email" />
-            <label>Message</label>
-            <textarea className="contact__input" name="message" />
             <input
-              className="bg-accent hover:cursor-pointer hover:bg-yellow-700 self-end p-2 rounded-md"
+              className="contact__input "
+              placeholder="Name"
+              type="text"
+              name="user_name"
+            />
+
+            <input
+              className="contact__input"
+              placeholder="Email"
+              type="email"
+              name="user_email"
+            />
+
+            <textarea
+              className="contact__input h-52"
+              placeholder="Message"
+              name="message"
+            />
+            <input
+              className="bg-accent hover:cursor-pointer hover:bg-white hover:text-accent font-bold  p-4 rounded-md"
               type="submit"
-              value="Send"
+              value="SEND"
             />
           </form>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export const HomePage = () => {
       id="home"
       className="flex-between flex-col md:px-80 py-10 bg-black  min-h-screen"
     >
-      <div className="flex-col    flex-between font-medium text-center w-full items-start     text-accent  text-8xl ">
+      <div className="flex-col scroll    flex-between font-medium text-center w-full items-start     text-accent  text-8xl ">
         <div className="pl-20  ">
           <h1>LUCAS</h1>
         </div>
