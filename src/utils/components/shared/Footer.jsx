c24ccc4c0085050444c3121d4fa66b66808f337c
Lucas Aguero <lucasbenja0376@gmail.com>
2023-11-09 11:52:27 -0300

    testing

--- src/utils/components/shared/Footer.jsx
+++ src/utils/components/shared/Footer.jsx
@@ -5,9 +5,9 @@ import { SiGithub, SiLinkedin } from 'react-icons/si';
 export const Footer = () => {
   return (
     <div className="bottom-0 h-[100px] w-full justify-between p-6 bg-black text-white flex-row flex">
-      <section className={`flex-col`}>copyright things</section>
-      <section className={`flex-col gap-4`}>
-        links of proffesional stuff
+      <section className="flex-col">copyright things</section>
+      <section className="flex-col gap-4">
+        links of proffesiona
         <a href="">
           <SiLinkedin />
         </a>
